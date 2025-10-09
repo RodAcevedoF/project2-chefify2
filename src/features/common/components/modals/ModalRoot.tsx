41d236a96fc1a8a1ec61f184092f27218c4ecf84
Rodrigo <raacevedof@gmail.com>
2025-10-09 21:03:09 +0200

    wip add profile modal

--- src/features/common/components/modals/ModalRoot.tsx
+++ src/features/common/components/modals/ModalRoot.tsx
@@ -2,10 +2,12 @@ import { AuthModal } from '@/features/auth/components/nav/auth.modal';
 import { useModalContext } from '@/contexts/modalContext/modal.context';
 import { Modal } from './Modal';
 import { RecipeModal } from '@/features/auth/components/nav/recipe.modal';
+import ProfileModal from '@/features/profile/components/ProfileModal';
 
 export const MODALS = {
 	auth: <AuthModal />,
 	recipe: <RecipeModal />,
+	profileEdit: <ProfileModal />,
 } as const;
 
 export const ModalRoot = () => {

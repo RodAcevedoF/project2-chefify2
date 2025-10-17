05907e3ffa6fd781064b9fab00a91099be15580b
Rodrigo <raacevedof@gmail.com>
2025-10-17 17:25:13 +0200

    chore clean modal

--- src/features/ingredients/services/ingredient.service.ts
+++ src/features/ingredients/services/ingredient.service.ts
@@ -1,18 +1,20 @@
 import chefifyAPI from '@/lib/api';
-import type { QueryParams } from '@/types/common.types';
-import type { IngredientDTO } from '@/types/ingredient.type';
+import type { CommonResponse, QueryParams } from '@/types/common.types';
+import type { Ingredient, IngredientDTO } from '@/types/ingredient.type';
 
 export const IngredientService = {
 	async create(data: IngredientDTO): Promise<void> {
 		await chefifyAPI.post('/ingredient', data);
 	},
 
-	async getIngredient(params: QueryParams) {
+	async getIngredient(
+		params: QueryParams,
+	): Promise<CommonResponse<Ingredient[]>> {
 		const ingredients = await chefifyAPI.get('/ingredient', { params });
 		return ingredients.data;
 	},
 
-	async getIngredientById(id: string) {
+	async getIngredientById(id: string): Promise<CommonResponse<Ingredient>> {
 		const ingredient = await chefifyAPI.get(`/ingredient/${id}`);
 		return ingredient.data;
 	},

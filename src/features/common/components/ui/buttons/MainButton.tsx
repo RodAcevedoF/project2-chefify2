c49561c559e4f006a9e712d66857ab77ef64f787
Rodrigo <raacevedof@gmail.com>
2025-11-02 21:43:27 +0100

    feat new button variants

--- src/features/common/components/ui/buttons/MainButton.tsx
+++ src/features/common/components/ui/buttons/MainButton.tsx
@@ -1,44 +1,70 @@
 import Button from '@mui/material/Button';
+import { Typography, useTheme } from '@mui/material';
 import type { ReactNode } from 'react';
 import type { LucideIcon } from 'lucide-react';
+import { CircularProgress } from '@mui/material';
+import { commonStyles } from '../../common.theme';
+import {
+	ButtonIconPositions,
+	ButtonTypes,
+	ButtonVariants,
+} from '@/types/common.types';
 
 interface MainButtonProps {
 	label?: string | ReactNode;
 	parentMethod?: () => void;
 	disabled?: boolean;
 	icon?: LucideIcon;
-	type?: 'button' | 'submit' | 'reset';
+	type?: ButtonTypes;
 	extraSx?: object;
 	iconSx?: object;
+	loader?: boolean;
+	variant?: ButtonVariants;
+	iconPos?: ButtonIconPositions;
 }
 
 export const ButtonUsage = (props: MainButtonProps) => {
-	const { label, parentMethod, disabled, icon: Icon, extraSx, iconSx } = props;
+	const {
+		label,
+		parentMethod,
+		disabled,
+		icon: Icon,
+		extraSx,
+		iconSx,
+		loader = true,
+		variant = ButtonVariants.DEFAULT,
+		iconPos = ButtonIconPositions.END,
+	} = props;
+	const cs = commonStyles(useTheme(), extraSx, disabled);
+
+	const getButtonStyles = () => {
+		switch (variant) {
+			case ButtonVariants.CANCEL:
+				return { ...cs.mainBtn, ...cs.cancelVariant };
+			case ButtonVariants.LIGHT:
+				return { ...cs.lightVariant };
+			case ButtonVariants.OUTLINED:
+				return { ...cs.outlinedVariant };
+			case ButtonVariants.DEFAULT:
+			default:
+				return cs.mainBtn;
+		}
+	};
+
 	return (
 		<Button
 			variant='outlined'
 			{...(parentMethod ? { onClick: parentMethod } : {})}
 			disabled={disabled}
-			type={props.type || 'button'}
-			sx={{
-				color: 'whitesmoke',
-				padding: '8px 16px',
-				background: '#033018',
-				borderRadius: 5,
-				borderColor: '#ffffeb',
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				gap: 1,
-				'&:hover': {
-					borderColor: '#0cea74',
-					background: 'inherit',
-					fontWeight: 'bold',
-				},
-				...extraSx,
-			}}>
-			{label}
-			{Icon && <Icon style={iconSx} />}
+			type={props.type || ButtonTypes.BUTTON}
+			sx={getButtonStyles()}>
+			{iconPos === ButtonIconPositions.START && Icon && <Icon style={iconSx} />}
+			{disabled && loader && (
+				<CircularProgress size={18} sx={{ color: '#ffffeb' }} />
+			)}
+			{disabled && <Typography sx={cs.typo}>{label}</Typography>}
+			{!disabled && label}
+			{iconPos === ButtonIconPositions.END && Icon && <Icon style={iconSx} />}
 		</Button>
 	);
 };

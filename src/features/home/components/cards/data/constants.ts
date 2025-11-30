74bd5692e0cc86091c9fc4b698fdb510e82f177f
Rodrigo <raacevedof@gmail.com>
2025-11-30 19:51:36 +0100

    fix routes

--- src/features/home/components/cards/data/constants.ts
+++ src/features/home/components/cards/data/constants.ts
@@ -3,21 +3,21 @@ export const landingCards = [
 		label: 'Get Started',
 		message:
 			'Create delicious recipes with ease. Bring your ideas to life with Chefify’s intuitive editor — fast, simple, and beautiful.',
-		imgUrl: '/default_1.webp',
+		imgUrl: '/landing-default/default_1.webp',
 		reverse: true,
 	},
 	{
 		label: 'Discover',
 		message:
 			'Discover creators who inspire you. Follow chefs, explore new cuisines, and build your own flavor-filled feed.',
-		imgUrl: '/default_3.webp',
+		imgUrl: '/landing-default/default_3.webp',
 		reverse: false,
 	},
 	{
 		label: 'Share',
 		message:
 			'Let AI spark your next dish. Get personalized recipe suggestions crafted from your tastes and cooking style.',
-		imgUrl: '/default_4.webp',
+		imgUrl: '/landing-default/default_4.webp',
 		reverse: true,
 	},
 ];
